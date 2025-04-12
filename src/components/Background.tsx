27ea5469bf8dc7daee162597e0cd6e7ff4822e39
Kimberly <144314381+huykimb@users.noreply.github.com>
2025-04-11 17:19:50 -1000

    Added color blocks for layout and added signup button

---
+++ src/components/Background.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
+import { Navbar, Container, Nav, Image, Offcanvas, Button, Row, Col } from 'react-bootstrap';
+
+const Background = () => (
+  <>
+    {/* orange block */}
+    <Container fluid className="top-bg p-0">
+      <div className="d-flex align-items-center gap-4 me-1 p-0">
+        <div>
+          <Image src="/school.png" fluid className="school-bg" />
+        </div>
+        {/* sign up text */}
+        <Row className="fullwidth-row align-items-center me-3">
+          <Col className="textright">
+            <p className="text1">A new way to enjoy the gym.</p>
+            <p className="text1">Meet with fellow students.</p>
+            <p className="text2">Sign up now to get started.</p>
+            <Button variant="outline-dark" className="border-2 main-signup">
+              Sign Up
+            </Button>
+          </Col>
+        </Row>
+      </div>
+    </Container>
+
+    {/* content block */}
+    <Container fluid className="py-5 green section1">
+      <h2>Section 1</h2>
+    </Container>
+
+    {/* features block */}
+    <Container fluid className="py-5 orange section2">
+      <h2>Section 2</h2>
+    </Container>
+  </>
+);
+
+export default Background;
